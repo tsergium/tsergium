5c8dfd148b546f898bcf592f70594ff85cfad81a
Sergiu Tomsa <sergiu.tomsa@rocket-internet.de>
2018-10-04 09:56:45 +0200

    Added react-bootstrap

--- components/header.js
+++ components/header.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import Link from "next/link";
+import Head from 'next/head';
 import '../assets/styles.scss';
 
 const Logo = "https://tsergium.ro/theme/tsergium/img/logo.jpg";
 
 const Header = () => (
   <div className="header">
+    <Head>
+      <title>Sergiu Tomsa | tsergium.ro</title>
+      <link
+        rel="stylesheet"
+        href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+        integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u"
+      />
+      <link
+        rel="stylesheet"
+        href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css"
+        integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp"
+      />
+    </Head>
     <div className="center_header navbar">
       <Link prefetch href="/">
         <a className="logo" href="/">
