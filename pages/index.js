e6f6c61b95e84274e81d635bbb7ddd3b46877bca
Sergiu Tomsa <sergiu.tomsa@rocket-internet.de>
2018-10-15 17:04:28 +0200

    Added animations

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
+import AOS from 'aos';
+import 'aos/src/sass/aos.scss';
 import Layout from '../components/layout';
 
 class App extends Component {
+  componentDidMount() {
+    AOS.init({
+      delay: 300,
+      duration: 1200,
+    });
+  }
+
   render() {
     return (
       <Layout>
         <div className="index_container">
           <div className="text">
-            <h1>Web Solutions and Mobile Applications</h1>
-            <p>
+            <h1 data-aos="slide-down">Web Solutions and Mobile Applications</h1>
+            <p data-aos="slide-up">
               Succesul adevarat este momentul in care oamenii inceteaza<br />
               sa te caute pe Facebook si incep sa te caute pe Google.
             </p>
