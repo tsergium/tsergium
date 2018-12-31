59ea8282210657d3f7d7cc64afacd1368b38f83d
Sergiu Tomsa <tsergium@gmail.com>
2018-12-31 21:00:26 +0100

    Test service worker

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import Layout from '../components/layout';
 import About from '../components/about';
 import Technologies from '../components/technologies';
@@ -6,6 +6,21 @@ import Portfolio from "../components/portfolio";
 import Hero from "../components/hero";
 
 class App extends Component {
+  componentDidMount() {
+    //This is the "Offline page" service worker
+    //Add this below content to your HTML page, or add the js file to your page at the very top to register service worker
+    if (navigator.serviceWorker.controller) {
+      console.log('[PWA Builder] active service worker found, no need to register')
+    } else {
+      //Register the ServiceWorker
+      navigator.serviceWorker.register('pwabuider-sw.js', {
+        scope: './'
+      }).then(function (reg) {
+        console.log('Service worker has been registered for scope:' + reg.scope);
+      });
+    }
+  }
+
   render() {
     return (
       <Layout>
---
+++ static/pwabuider-sw.js
@@ -0,0 +1,16 @@
+//This is the "Offline page" service worker
+
+//Install stage sets up the offline page in the cache and opens a new cache
+self.addEventListener('install', function(event) {
+    var offlinePage = new Request('offline.html');
+    event.waitUntil(
+        fetch(offlinePage).then(function(response) {
+            return caches.open('pwabuilder-offline').then(function(cache) {
+                console.log('[PWA Builder] Cached offline page during Install'+ response.url);
+                return cache.put(offlinePage, response);
+            });
+        }));
+});
+
+//If any fetch fails, it will show the offline page.
+//Maybe this should be limited to HTML documents?
